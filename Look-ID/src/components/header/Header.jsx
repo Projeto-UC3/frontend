dc6b62cb5ded819bf810aa9deaad90d723d2623c
iarley <iarleysouza615@gmail.com>
2024-10-17 12:24:53 -0300

    add image

--- Look-ID/src/components/header/Header.jsx
+++ Look-ID/src/components/header/Header.jsx
@@ -1,23 +1,33 @@
 import '../Styles/Header.css'
 
 function Header() {
-    return(
-        <header>
-            <div>
-                <img src="" alt="" />
-            </div>
-            <nav>
-                <ul>
-                    <li>Quem somos</li>
-                    <li>Serviços</li>
-                    <li>Contato</li>
-                </ul>
-            </nav>
-            <div className='button'>
-                <div className='button-1'>Cadastre-se</div>
-                <div className='button-2'>Entrar</div>
-            </div>
-        </header>
+    return (
+        <>
+            <header>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <nav>
+                    <ul>
+                        <li>Quem somos</li>
+                        <li>Serviços</li>
+                        <li>Contato</li>
+                    </ul>
+                </nav>
+                <div className='button'>
+                    <div className='button-1'>Cadastre-se</div>
+                    <div className='button-2'>Entrar</div>
+                </div>
+            </header>
+            <main>
+                <div>
+                    <h2></h2>
+                </div>
+                <div>
+                    <img src="assets/images/alunos-img.svg" alt="" />
+                </div>
+            </main>
+        </>
     )
 }
 export default Header
