07124fbaa157816396795fe420f8ddbd2f1bbbfe
iarley <iarleysouza615@gmail.com>
2024-10-24 16:15:50 -0300

    quase finalizada

--- Look-ID/src/App.jsx
+++ Look-ID/src/App.jsx
@@ -1,12 +1,18 @@
 import NavBar from './components/navBar/NavBar'
 import './App.css'
-import Section from './components/section/Section'
-import Header from './components/header/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/header/Header';
+import Section from './components/section/Section';
 function App() {
   return (
-    <main>
-      <NavBar />
-      <Section/>
+
+    <main className='h1-h1'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='Doe' element={<NavBar/>} />
+        </Routes>
+      </Router>
     </main>
   )
 }
