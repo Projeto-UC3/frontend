d5f2ff81969c8b9b14dfb2ecc470e5ad161ee409
iarley <iarleysouza615@gmail.com>
2024-10-28 15:42:20 -0300

    update

--- Look-ID/src/App.jsx
+++ Look-ID/src/App.jsx
@@ -6,7 +6,6 @@ import Header from './components/header/Header';
 import NavBar from './components/navBar/NavBar';
 import Servicos from "./components/Servicos/Servicos";
 import QuemSomos from "./components/QuemSomos/QuemSomos";
-import FreqAluno from "./components/freq_aluno/FreqAluno";
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           <Route path='/' element={<Corpo/>} />
           <Route path='Servicos' element={<Servicos/>} />
           <Route path='Somos' element={<QuemSomos/>} />
-          <Route path='Frequencia' element={<FreqAluno/>} />
         </Routes>
         <Footer/>
       </Router>
--- Look-ID/src/components/freq_aluno/Grafico.jsx
+++ Look-ID/src/components/freq_aluno/Grafico.jsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
+// import React from 'react';
 
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+// import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
 
-const FaltasChart = () => {
+// ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-    const data = {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago'],
-        datasets: [
-            {
-                label: 'Faltas',
-                data: [0, 1, 0, 2, 0, 5, 1, 2],
-                backgroundColor: '#90ee90', //cor da barrinha do grafico
-                borderRadius: 5, // borda
-                barThickness: 40, // largura
-            },
-        ],
-    };
+// const FaltasChart = () => {
+
+//     const data = {
+//         labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago'],
+//         datasets: [
+//             {
+//                 label: 'Faltas',
+//                 data: [0, 1, 0, 2, 0, 5, 1, 2],
+//                 backgroundColor: '#90ee90', //cor da barrinha do grafico
+//                 borderRadius: 5, // borda
+//                 barThickness: 40, // largura
+//             },
+//         ],
+//     };
 
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: false,
-            },
-        },
-        scales: {
-            x: {
-                grid: {
-                    display: false,
-                },
-            },
-            y: {
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 1,
-                },
-                grid: {
-                    display: false,
-                },
-            },
-        },
-    };
+//     const options = {
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 display: false,
+//             },
+//         },
+//         scales: {
+//             x: {
+//                 grid: {
+//                     display: false,
+//                 },
+//             },
+//             y: {
+//                 ticks: {
+//                     beginAtZero: true,
+//                     stepSize: 1,
+//                 },
+//                 grid: {
+//                     display: false,
+//                 },
+//             },
+//         },
+//     };
 
-    return (
-        <div style={styles.chartContainer}>
-            <Bar data={data} options={options} />
-        </div>
-    );
-};
+//     return (
+//         <div style={styles.chartContainer}>
+//             <Bar data={data} options={options} />
+//         </div>
+//     );
+// };
 
-const styles = {
-    chartContainer: {
-        marginTop: '15%',
-        padding: '2%',
-        height: '100%',
-    },
-};
+// const styles = {
+//     chartContainer: {
+//         marginTop: '15%',
+//         padding: '2%',
+//         height: '100%',
+//     },
+// };
 
-export default FaltasChart;
+// export default FaltasChart;
