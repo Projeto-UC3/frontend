39075b30073a68289169abe7638551b671743bc6
iarley <iarleysouza615@gmail.com>
2024-10-30 23:42:28 -0300

    update

--- Look-ID/src/components/footer/Footer.jsx
+++ Look-ID/src/components/footer/Footer.jsx
@@ -8,6 +8,7 @@ function Footer() {
             </div>
             <div className='footeerr'>
                 <ul>
+                    <li>Quem somos</li>
                     <li>Sobre nós</li>
                     <li>Funcionalidades</li>
                     <li>Termo de uso</li>
@@ -18,10 +19,15 @@ function Footer() {
             <div></div>
             <div className='footeerr'>
                 <ul>
+                    <li>Telefone:</li>
                     <li>(11) 2632-4489</li>
                     <li>(11) 1028-5678</li>
+                    <br />
+                    <li>Contato:</li>
                     <li>E-mail: brotomais@gmail.com.br</li>
-                    <li>Rua Tito, 54 - Lapa, SP </li>
+                    <li>Endereço:</li>
+                    <br />
+                    <li>Rua Tito, 54 - Lapa, SP</li>
                     <li>Política de privacidade</li>
                 </ul>
             </div>
